27455be851d9d5a7c8fa9df0a7656a2f2d3d61c2
VD <vdfree448@protonmail.com>
2023-07-21 12:38:02 +0300

    after delete, added new dependencies which are working fine

--- app/src/main/java/com/example/module_3_lesson_6_hw_2_compose/model/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.module_3_lesson_6_hw_2_compose.model
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [LightOfRoom::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun getLightOfRoomDao(): LightOfRoomDao
-}
